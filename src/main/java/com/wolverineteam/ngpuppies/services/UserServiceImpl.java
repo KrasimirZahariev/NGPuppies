dca36aa460370cf52ca0199b66c7ef0489a06d23
KrasimirZahariev <k.zahariew@abv.bg>
2018-08-16 19:58:10 +0300

    add user service functionality

--- src/main/java/com/wolverineteam/ngpuppies/services/UserServiceImpl.java
+++ src/main/java/com/wolverineteam/ngpuppies/services/UserServiceImpl.java
@@ -1,6 +1,45 @@
 package com.wolverineteam.ngpuppies.services;
 
+import com.wolverineteam.ngpuppies.data.base.UserRepository;
+import com.wolverineteam.ngpuppies.models.User;
 import com.wolverineteam.ngpuppies.services.base.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User getById(int id) {
+        return userRepository.getById(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userRepository.getAll();
+    }
+
+    @Override
+    public void update(int id, User user) {
+        userRepository.update(id, user);
+    }
+
+    @Override
+    public void create(User user) {
+        userRepository.create(user);
+    }
+
+    @Override
+    public void delete(int id) {
+        userRepository.delete(id);
+    }
 }
--- src/main/java/com/wolverineteam/ngpuppies/services/base/UserService.java
+++ src/main/java/com/wolverineteam/ngpuppies/services/base/UserService.java
@@ -1,4 +1,18 @@
 package com.wolverineteam.ngpuppies.services.base;
 
+import com.wolverineteam.ngpuppies.models.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    User getById(int id);
+
+    List<User> getAll();
+
+    void update(int id, User user);
+
+    void create(User user);
+
+    void delete(int id);
 }
