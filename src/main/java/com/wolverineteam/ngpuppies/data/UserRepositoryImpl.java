cb75b050b4310cc06d2059d28d428ff6a9ea988f
KrasimirZahariev <k.zahariew@abv.bg>
2018-08-16 19:51:40 +0300

    add user repo functionality

--- src/main/java/com/wolverineteam/ngpuppies/data/UserRepositoryImpl.java
+++ src/main/java/com/wolverineteam/ngpuppies/data/UserRepositoryImpl.java
@@ -1,6 +1,85 @@
 package com.wolverineteam.ngpuppies.data;
 
 import com.wolverineteam.ngpuppies.data.base.UserRepository;
+import com.wolverineteam.ngpuppies.models.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class UserRepositoryImpl implements UserRepository {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public UserRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public User getById(int id) {
+        User user = null;
+        try(Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            user = session.get(User.class, id);
+            session.getTransaction().commit();
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return user;
+    }
+
+    @Override
+    public List<User> getAll() {
+        List<User> users = new ArrayList<>();
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            users = session.createQuery("from User", User.class).list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return users;
+    }
+
+    @Override
+    public void update(int id, User user) {
+        User oldUser = getById(id);
+        oldUser.setUsername(user.getUsername());
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.update(oldUser);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void create(User user) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        User user = getById(id);
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.delete(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/main/java/com/wolverineteam/ngpuppies/data/base/UserRepository.java
+++ src/main/java/com/wolverineteam/ngpuppies/data/base/UserRepository.java
@@ -1,4 +1,18 @@
 package com.wolverineteam.ngpuppies.data.base;
 
+import com.wolverineteam.ngpuppies.models.User;
+
+import java.util.List;
+
 public interface UserRepository {
+
+    User getById(int id);
+
+    List<User> getAll();
+
+    void update(int id, User currency);
+
+    void create(User currency);
+
+    void delete(int id);
 }
