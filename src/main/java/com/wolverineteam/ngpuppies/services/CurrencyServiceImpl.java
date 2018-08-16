847e885ae2da412563b86bbfa1d67730c79ea9dd
KrasimirZahariev <k.zahariew@abv.bg>
2018-08-16 12:29:51 +0300

    add currency service functionality

--- src/main/java/com/wolverineteam/ngpuppies/services/CurrencyServiceImpl.java
+++ src/main/java/com/wolverineteam/ngpuppies/services/CurrencyServiceImpl.java
@@ -1,6 +1,45 @@
 package com.wolverineteam.ngpuppies.services;
 
+import com.wolverineteam.ngpuppies.data.base.CurrencyRepository;
+import com.wolverineteam.ngpuppies.models.Currency;
 import com.wolverineteam.ngpuppies.services.base.CurrencyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class CurrencyServiceImpl implements CurrencyService {
+
+    @Autowired
+    private CurrencyRepository currencyRepository;
+
+    public CurrencyServiceImpl(CurrencyRepository currencyRepository) {
+        this.currencyRepository = currencyRepository;
+    }
+
+    @Override
+    public Currency getById(int id) {
+        return currencyRepository.getById(id);
+    }
+
+    @Override
+    public List<Currency> getAll() {
+        return currencyRepository.getAll();
+    }
+
+    @Override
+    public void update(int id, Currency currency) {
+        currencyRepository.update(id, currency);
+    }
+
+    @Override
+    public void create(Currency currency) {
+        currencyRepository.create(currency);
+    }
+
+    @Override
+    public void delete(int id) {
+        currencyRepository.delete(id);
+    }
 }
--- src/main/java/com/wolverineteam/ngpuppies/services/base/CurrencyService.java
+++ src/main/java/com/wolverineteam/ngpuppies/services/base/CurrencyService.java
@@ -1,4 +1,18 @@
 package com.wolverineteam.ngpuppies.services.base;
 
+import com.wolverineteam.ngpuppies.models.Currency;
+
+import java.util.List;
+
 public interface CurrencyService {
+
+    Currency getById(int id);
+
+    List<Currency> getAll();
+
+    void update(int id, Currency currency);
+
+    void create(Currency currency);
+
+    void delete(int id);
 }
