ea4f2d3d9de2ed48af232c285d8d38ebe5bddd9f
KrasimirZahariev <k.zahariew@abv.bg>
2018-08-16 12:11:35 +0300

    add currencyrepo functionality

--- src/main/java/com/wolverineteam/ngpuppies/data/CurrencyRepositoryImpl.java
+++ src/main/java/com/wolverineteam/ngpuppies/data/CurrencyRepositoryImpl.java
@@ -1,6 +1,85 @@
 package com.wolverineteam.ngpuppies.data;
 
 import com.wolverineteam.ngpuppies.data.base.CurrencyRepository;
+import com.wolverineteam.ngpuppies.models.Currency;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class CurrencyRepositoryImpl implements CurrencyRepository {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public CurrencyRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Currency getById(int id) {
+        Currency currency = null;
+        try(Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            currency = session.get(Currency.class, id);
+            session.getTransaction().commit();
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return currency;
+    }
+
+    @Override
+    public List<Currency> getAll() {
+        List<Currency> currencies = new ArrayList<>();
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            currencies = session.createQuery("from Currency", Currency.class).list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return currencies;
+    }
+
+    @Override
+    public void update(int id, Currency currency) {
+        Currency oldCurrency = getById(id);
+        oldCurrency.setCurrency(currency.getCurrency());
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.update(oldCurrency);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void create(Currency currency) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.save(currency);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        Currency currency = getById(id);
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.delete(currency);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/main/java/com/wolverineteam/ngpuppies/data/base/CurrencyRepository.java
+++ src/main/java/com/wolverineteam/ngpuppies/data/base/CurrencyRepository.java
@@ -1,4 +1,18 @@
 package com.wolverineteam.ngpuppies.data.base;
 
+import com.wolverineteam.ngpuppies.models.Currency;
+
+import java.util.List;
+
 public interface CurrencyRepository {
+
+    Currency getById(int id);
+
+    List<Currency> getAll();
+
+    void update(int id, Currency currency);
+
+    void create(Currency currency);
+
+    void delete(int id);
 }
