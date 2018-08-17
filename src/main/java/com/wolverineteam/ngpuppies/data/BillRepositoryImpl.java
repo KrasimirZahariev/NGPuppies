f35bc807dcdbf282542770a4edfc65de61d175dd
KrasimirZahariev <k.zahariew@abv.bg>
2018-08-17 09:04:07 +0300

    add some methods in Bills repo without implementation

--- src/main/java/com/wolverineteam/ngpuppies/data/BillRepositoryImpl.java
+++ src/main/java/com/wolverineteam/ngpuppies/data/BillRepositoryImpl.java
@@ -1,8 +1,26 @@
 package com.wolverineteam.ngpuppies.data;
 
 import com.wolverineteam.ngpuppies.data.base.BillRepository;
+import com.wolverineteam.ngpuppies.models.Bill;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public class BillRepositoryImpl implements BillRepository{
 
+    @Override
+    public List<Bill> getAllNotPaid(int bankId) {
+        return null;
+    }
+
+    @Override
+    public void pay(List<Bill> bills) {
+
+    }
+
+    @Override
+    public List<Bill> getAllPaidSorted(int bankId) {
+        return null;
+    }
 }
--- src/main/java/com/wolverineteam/ngpuppies/data/base/BillRepository.java
+++ src/main/java/com/wolverineteam/ngpuppies/data/base/BillRepository.java
@@ -7,7 +7,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@Repository
+
 public interface BillRepository {
 
+    //They must have access to bill payment module where they can pay a particular bill
+    // (or selected list of bills) for their subscribers
+    List<Bill> getAllNotPaid(int bankId);
+    void pay(List<Bill> bills);
+
+
+
+    //A client should be able to see a history of the payments for its subscribers sorted
+    // descending by the date of payment
+    List<Bill> getAllPaidSorted(int bankId);
+
+
+    //A client should be able to see the average and max amount of money
+    // payed for a subscriber for a defined period of time
+
+
+    //A client should be able to see a list of the services the client has paid for
+
+    //Top 10 subscribers with the biggest amount of money payed.
+    // If there are payments with amounts in different currencies,
+    // they should be all converted to BGN (using a static conversion rate) and summed
+
+
+    //The 10 most recent payments for the particular client for all the subscribers
 }
