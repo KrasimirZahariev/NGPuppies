ade1d7359f8fd356033618a90aee53b380a56898
KrasimirZahariev <k.zahariew@abv.bg>
2018-08-20 11:35:07 +0300

    add spring security and thymeleaf for tests

---
+++ src/main/java/com/wolverineteam/ngpuppies/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.wolverineteam.ngpuppies.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        //Configuration for testing purposes
+        http.authorizeRequests().anyRequest().permitAll();
+
+//        http.authorizeRequests()
+//                .antMatchers("/").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                    .loginPage("/login")
+//                    .loginProcessingUrl("/authenticate")
+//                    .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(User.withUsername("admin").password("{noop}admin").roles("CLIENT", "ADMIN"))
+                .withUser(User.withUsername("client").password("{noop}client").roles("CLIENT"));
+    }
+}
--- src/main/java/com/wolverineteam/ngpuppies/web/LogInController.java
+++
@@ -1,9 +0,0 @@
-package com.wolverineteam.ngpuppies.web;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/logIn")
-public class LogInController {
-}
