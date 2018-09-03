6d371213ad2e90205509278df6e28c599adafa36
Borislav Borisov <borislav.borisov32@gmail.com>
2018-09-03 18:23:15 +0300

    Add RoleServiceTest and test loadRoleByRoleName_ReturnCorrectRole

---
+++ src/test/java/com/wolverineteam/ngpuppies/ServiceTests/RoleServiceTest.java
@@ -0,0 +1,33 @@
+package com.wolverineteam.ngpuppies.ServiceTests;
+
+import com.wolverineteam.ngpuppies.data.base.RoleRepository;
+import com.wolverineteam.ngpuppies.models.Role;
+import com.wolverineteam.ngpuppies.services.RoleServiceImpl;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RoleServiceTest {
+
+    @Mock
+    RoleRepository roleRepository;
+
+    @InjectMocks
+    RoleServiceImpl roleService;
+
+    @Test
+    public void loadRoleByRoleName_ReturnCorrectRole(){
+        Mockito.when(roleRepository.loadRoleByRoleName("ROLE_ADMIN"))
+                .thenReturn(new Role("ROLE_ADMIN"));
+
+        Role result = roleService.loadRoleByRoleName("ROLE_ADMIN");
+
+        Assert.assertEquals("ROLE_ADMIN", result.getRole());
+    }
+
+}
